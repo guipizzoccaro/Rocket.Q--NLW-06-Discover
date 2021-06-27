6c11da1d2c562dbd5f8458fa15f8a84305308c2d
Guilherme Pizzoccaro <guilherme.pizzoccaro@outlook.com>
2021-06-27 12:29:05 -0300

    App Finalized
    Final of 5th class
    all done.

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,11 +1,31 @@
 const Database = require('../db/config')
 
 module.exports = {
-    index(req, res) {
+    async index(req, res) {
+        const db = await Database()
         const roomId = req.params.room
         const questionId = req.params.question
         const action = req.params.action
         const password = req.body.password
+
+        // Verificar a senha
+        // db.get traz um dado só
+        const verifyRoom = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`)
+        if (verifyRoom.pass == password) {
+            if (action == "delete") {
+
+                await db.run(`DELETE FROM questions WHERE id=${questionId}`)
+
+            } else if (action == "check") {
+
+                await db.run(`UPDATE questions SET read = 1 WHERE id=${questionId}`)
+
+            }
+            res.redirect(`/room/${roomId}`)
+        } else {
+            res.render(`passIncorrect`, { roomId: roomId })
+        }
+
 
 
     },
